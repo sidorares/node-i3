a31bc2c399d6fed1adab69a77ef6586237a175d6
Andrey Sidorov <sidorares@yandex.ru>
2014-08-28 19:13:50 +1000

    add other 5 command types

--- index.js
+++ index.js
@@ -7,5 +7,30 @@ module.exports.createClient = module.exports.connect = function(options) {
 
 I3IpcClient.prototype.command = function(arg, cb) {
   this.message(0, arg, cb);
-}
+};
+
+I3IpcClient.prototype.workspaces = function(cb) {
+  this.message(1, null, cb);
+};
+
+I3IpcClient.prototype.sbscribe = function(events) {
+  this.message(2, events, cb);
+};
+
+I3IpcClient.prototype.outputs = function(cb) {
+  this.message(3, null, cb);
+};
+
+I3IpcClient.prototype.tree = function(cb) {
+  this.message(4, null, cb);
+};
+
+I3IpcClient.prototype.marks = function(cb) {
+  this.message(5, null, cb);
+};
+
+I3IpcClient.prototype.barConfig = function(cb) {
+  this.message(6, null, cb);
+};
+
 module.exports.I3IpcClient = I3IpcClient;
